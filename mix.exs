79799f38f7de87f70b36cb336aa3d5dce19e1ea7
Brian E McElaney <brian.e.mcelaney@gmail.com>
2020-01-16 20:29:27 -0500

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Lunch.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
 
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
     ]
   end
 
